dcc7253bebb24fee5847e7acea11b2e55669fe05
Florensius <florensiusxodc@gmail.com>
2020-08-26 15:50:30 +0700

    improve user login

--- backend_dashboard/app.py
+++ backend_dashboard/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
-from security import authenticate, identity
-from resources.user import UserRegister
+from db import db
+from resources.user import UserRegister, UserLogin, User, UserLogout
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -19,11 +20,60 @@ cors = CORS(app)
 def create_tables():
     db.create_all()
 
-jwt = JWT(app, authenticate, identity )
+jwt = JWTManager(app)
+
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1: # Instead of hard-coding, you should read from a config file or a database
+        return {'is_admin': True}
+    return {'is_admin': False}
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['jti'] in BLACKLIST
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+@jwt.invalid_token_loader
+def invalid_token_loader(error):
+    return jsonify({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+@jwt.unauthorized_loader
+def unauthorized_loader(error):
+    return jsonify({
+        'description': 'Request does not contain an access token.',
+        'error': 'authorization_required'
+    }), 401
+
+@jwt.needs_fresh_token_loader
+def needs_fresh_token_loader():
+    return jsonify({
+        'description': 'The token is not fresh.',
+        'error': 'fresh_token_required'
+    }), 401
+
+@jwt.revoked_token_loader
+def revoked_token_loader():
+    return jsonify({
+        'description': 'The token has been revoked.',
+        'error': 'token_revoked'
+    }), 401
+
+
 
 api.add_resource(UserRegister, '/register')
+api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
+api.add_resource(User, '/user/<int:user_id>')
 
 if __name__ == '__main__':
-    from db import db
     db.init_app(app)
     app.run(debug=True)
--- backend_dashboard/resources/user.py
+++ backend_dashboard/resources/user.py
@@ -1,23 +1,32 @@
-import sqlite3
 from flask_restful import Resource, reqparse
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import (
+    create_access_token, 
+    create_refresh_token, 
+    jwt_refresh_token_required,
+    get_jwt_identity,
+    jwt_required,
+    get_raw_jwt
+)
+
 from models.user import UserModel
+from blacklist import BLACKLIST
+
+user_parser = reqparse.RequestParser()
+user_parser.add_argument('username', 
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
+user_parser.add_argument('password',
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
 
 class UserRegister(Resource):
-
-    parser = reqparse.RequestParser()
-    parser.add_argument('username', 
-        type=str,
-        required=True,
-        help="This field cannot be blank."
-    )
-    parser.add_argument('password',
-        type=str,
-        required=True,
-        help="This field cannot be blank."
-    )
-
     def post(self):
-        data = UserRegister.parser.parse_args()
+        data = user_parser.parse_args()
 
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 409
@@ -26,3 +35,45 @@ class UserRegister(Resource):
         user.save_to_db()
 
         return {"message": "User created successfully."}, 201
+
+class User(Resource):
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found'}, 404
+        return user.json()
+
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found'}, 404
+        user.delete_from_db()
+        return {'message': 'User deleted.'}, 200
+
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        data = user_parser.parse_args()
+
+        user = UserModel.find_by_username(data['username'])
+
+        if user:
+            if user and safe_str_cmp(user.password, data['password']):
+                access_token = create_access_token(identity=user.id, fresh=True)
+                refresh_token = create_refresh_token(user.id)
+                return {
+                    'access_token': access_token,
+                    'refresh_token': refresh_token
+                }, 200
+            return {'message': 'Your password is wrong'}, 401
+        return {'message': 'Your username is not found'}
+
+class UserLogout(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']  # jti is "JWT ID", a unique identifier for a JWT
+        BLACKLIST.add(jti)
+        return {'message': 'Successfully logout.'}
+
